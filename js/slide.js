d8e6cbf29ccf251350b779381960a10ba2271cec
havietduc91 <havietduc1991>
2013-07-16 09:41:43 +0700

    add file to bongda

---
+++ js/slide.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	
+	$("#panel").delay(3000).slideUp("slow");	
+	
+	// Expand Panel
+	$("#open").click(function(){
+		$("div#panel").slideDown("slow");
+	
+	});	
+	
+	// Collapse Panel
+	$("#close").click(function(){
+		$("div#panel").slideUp("slow");	
+	});		
+	
+	// Switch buttons from "Log In | Register" to "Close Panel" on click
+	$("#toggle a").click(function () {
+		$("#toggle a").toggle();
+	});		
+		
+});
